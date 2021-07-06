1ba48150c156294cb47e3cfb439973e3d3e0a16d
bhargavasatyamani <bhargav4397@gmail.com>
2021-07-06 20:59:53 +0530

    Changed the environment file for prod

--- AppDev/angular/src/environments/environment.prod.ts
+++ AppDev/angular/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAIdcg7I5QOND8yJ2lY2cXUFfmlgP75rf8",
+    authDomain: "bbsm-angular-app-8329a.firebaseapp.com",
+    databaseURL: "https://bbsm-angular-app-8329a-default-rtdb.firebaseio.com",
+    projectId: "bbsm-angular-app-8329a",
+    storageBucket: "bbsm-angular-app-8329a.appspot.com",
+    messagingSenderId: "4391717775",
+    appId: "1:4391717775:web:aad5e96c79b0239e8599fa"
+  }
 };
