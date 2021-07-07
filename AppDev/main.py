45ff850335ee0104e5542c45dc3a7bb7fa4f1756
bhargavasatyamani <bhargav4397@gmail.com>
2021-07-07 11:10:52 +0530

    Added Dockerfile

--- AppDev/main.py
+++ AppDev/main.py
@@ -4,10 +4,24 @@ from flask_cors import CORS
 from src.firestore_api import firestore_api
 from src.storage_api import storage_api
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='angular/dist/angular')
 app.register_blueprint(firestore_api,url_prefix='/api')
 app.register_blueprint(storage_api,url_prefix='/api')
+angular = Blueprint('angular',__name__,template_folder='angular/dist/angular')
+app.register_blueprint(angular)
 CORS(app)
+
+@app.route('/assets/<path:filename>')
+def custom_static_for_assets(filename):
+    return send_from_directory('angular/dist/angular/assets',filename)
+
+@app.route('/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('angular/dist/angular/', filename)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     #app.run('127.0.0.1',port='8080',debug=True)
--- AppDev/src/utils/secret_manager.py
+++ AppDev/src/utils/secret_manager.py
@@ -6,8 +6,8 @@ sm_client = secretmanager.SecretManagerServiceClient()
 def get_secret(project_id, secret_id, version_id='latest'):
     try:
         name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
-        #response = sm_client.access_secret_version(request={"name":name})
-        response = sm_client.access_secret_version(name)
+        response = sm_client.access_secret_version(request={"name":name})
+        #response = sm_client.access_secret_version(name)
         return response.payload.data.decode()
     except Exception as e:
         traceback.print_exc()
